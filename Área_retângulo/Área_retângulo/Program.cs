4fdf2bd9df270f4b765b1abc6b6000c54aaa1521
luiz00770 <almeida.filipe@aluno.ifsp.edu.br>
2026-08-24 20:33:59 -0300

    Add files via upload

---
+++ Área_retângulo/Área_retângulo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Área_retângulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Área ret = new Área();
+
+            ret.setbase(EntrarInt("Digite o valor de base do retângulo: "));
+            ret.setaltura(EntrarInt("Digite o valor da altura do retângulo: "));
+
+            ret.Calcular();
+            Console.WriteLine("A área do retângulo é: {0}",
+                ret.getresultado());
+        }
+
+        static int EntrarInt(string mensagem)
+        { 
+            Console.Write(mensagem);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ Área_retângulo/Área_retângulo/Área.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Área_retângulo
+{
+    internal class Área
+    {
+        private int baze;
+        private int altura;
+        private int resultado;
+
+        public Área()
+        { 
+            this.baze = 0;
+            this.altura = 0;
+            this.resultado = 0;
+        }
+        public Área(int baze, int altura)
+        {
+            this.baze = baze;
+            this.altura = altura;
+            this.resultado = 0;
+
+        }
+
+        public void setbase(int baze)
+        {
+            this.baze = baze;
+        }
+        public void setaltura(int altura)
+        {
+            this.altura = altura;
+        }
+        public int getbase()
+        {
+            return this.baze;
+        }
+        public int getaltura()
+        {
+            return this.altura;
+                
+        }
+        public int getresultado()
+        {
+            return this.resultado;
+
+        }
+
+        public void Calcular()
+        {
+            this.resultado = this.baze * this.altura;
+
+        }
+
+    }
+}
