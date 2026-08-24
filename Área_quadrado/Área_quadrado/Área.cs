c0b1e2c9ce0560a4e6be450ab1c9237907b130d1
luiz00770 <almeida.filipe@aluno.ifsp.edu.br>
2026-08-24 20:59:31 -0300

    Add files via upload

---
+++ Área_quadrado/Área_quadrado/Área.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Área_quadrado
+{
+    internal class Área
+    {
+        private int aresta;
+        private int resultado;
+
+        public Área()
+        {
+            this.aresta = 0;
+            this.resultado = 0;
+        }
+
+        public Área(int aresta, int resultado)
+        {
+            this.aresta = aresta;
+            this.resultado = 0;
+        }
+
+        public void SetAresta(int aresta)
+        { 
+            this.aresta = aresta;
+        }
+        public int GetAresta() 
+        {
+            return this.aresta;        
+        }
+
+        public int GetResultado() 
+        {
+            return resultado;
+        }
+
+        public void Calcular()
+        {
+            this.resultado = this.aresta * this.aresta;     
+        }
+    }
+}
