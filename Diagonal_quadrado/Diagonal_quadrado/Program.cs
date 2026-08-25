ea01835aa88fd5964fdd35d86aadafbd38ec8c28
luiz00770 <almeida.filipe@aluno.ifsp.edu.br>
2026-08-24 21:05:55 -0300

    Add files via upload

---
+++ Diagonal_quadrado/Diagonal_quadrado/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagonal_quadrado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Área quad = new Área();
+
+            quad.SetDiag(EntrarInt("Digite o valor da aresta do quadrado: "));
+
+            quad.Calcular();
+            Console.WriteLine("A área do quadrado é: {0}",
+                quad.GetResultado());
+        }
+
+        static int EntrarInt(string mensagem)
+        {
+            Console.Write(mensagem);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+
+}
---
+++ Diagonal_quadrado/Diagonal_quadrado/Área.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagonal_quadrado
+{
+    internal class Área
+    {
+        private double diag;
+        private double resultado;
+
+        public Área()
+        {
+            this.diag = 0;
+            this.resultado = 0;
+        }
+
+        public Área(double diag, double resultado)
+        {
+            this.diag = diag;
+            this.resultado = 0;
+        }
+
+        public void SetDiag(double diag)
+        {
+            this.diag = diag;
+        }
+        public double GetAresta()
+        {
+            return this.diag;
+        }
+
+        public double GetResultado()
+        {
+            return resultado;
+        }
+
+        public void Calcular()
+        {
+            this.resultado = Math.Sqrt(this.diag) * Math.Sqrt(this.diag);
+        }
+    }
+}
