2e8f9ebaef5ba42a3325db1ad04c92fa13687e3c
luiz00770 <almeida.filipe@aluno.ifsp.edu.br>
2026-08-31 19:57:35 -0300

    Add files via upload

---
+++ Área_triângulo/Área_triângulo/Area.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Área_triângulo
+{
+    internal class Area
+    {
+        private double baze;
+        private double altura;
+        private double resultado;
+
+
+        public Area()
+        {
+            this.baze = 0;
+            this.altura = 0;
+            this.resultado = 0;
+
+        }
+
+        public void setbaze(double baze)
+        { 
+            this.baze = baze;
+        }
+        public void setaltura(double altura)
+        {
+            this.altura = altura;
+        }
+        public double getbaze()
+        {
+            return this.baze; 
+        }
+        public double getaltura() 
+        {
+            return this.altura;        
+        }
+        public double getresultado()
+        {
+            return this.resultado; 
+        }
+
+        public void Calcular()
+        {
+            this.resultado = (this.baze * this.altura) / 2;
+        }
+    }
+}
---
+++ Área_triângulo/Área_triângulo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Área_triângulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Area tri = new Area();
+
+            tri.setbaze(entrarDouble("Digite a base do triângulo: "));
+            tri.setaltura(entrarDouble("Digite a altra do triângulo: "));
+
+            tri.Calcular();
+                Console.WriteLine("A área do triângulo é de: {0}",
+                    tri.getresultado());
+
+        }
+
+        static double entrarDouble(string mensagem)
+        { 
+            Console.Write(mensagem);
+            return double.Parse(Console.ReadLine());
+        }
+    }
+}
