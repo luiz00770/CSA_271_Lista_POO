dff1c5516bb55948a470b8cf230384a3675f5d63
luiz00770 <almeida.filipe@aluno.ifsp.edu.br>
2026-08-31 20:22:56 -0300

    Add files via upload

---
+++ Conversor_km_milhas/Conversor_km_milhas/Conversao.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversos_km_milhas
+{
+    internal class Conversao
+    {
+        private double milhas;
+        private double resultado;
+
+
+        public Conversao()
+        {
+            this.milhas = 0;
+            this.resultado = 0;
+
+        }
+
+        public void setmilhas(double milhas)
+        {
+            this.milhas = milhas;
+        }
+        public double getmilhas()
+        {
+            return this.milhas;
+        }
+        public double getresultado()
+        {
+            return this.resultado;
+        }
+
+        public void Calcular()
+        {
+            this.resultado = this.milhas*1.852;
+        }
+    }
+}
---
+++ Conversor_km_milhas/Conversor_km_milhas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversos_km_milhas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conversao valor = new Conversao();
+
+            valor.setmilhas(Msg("Digite a distância em milhas: "));
+
+            valor.Calcular();
+                Console.WriteLine("O valor em quilômetros dessa distância é de: {0}km",
+                    valor.getresultado());
+
+        }
+        static double Msg(string msg)
+        { 
+            Console.Write(msg);
+            return double.Parse(Console.ReadLine());
+        }
+    }
+}
