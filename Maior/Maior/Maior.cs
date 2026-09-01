de97e56bd79edc82efb41cf3951557ce1e855eeb
luiz00770 <almeida.filipe@aluno.ifsp.edu.br>
2026-08-31 21:12:40 -0300

    Add files via upload

---
+++ Maior/Maior/Maior.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maior
+{
+    internal class Maior
+    {
+        private double n1;
+        private double n2;
+        private string resultado;
+
+
+
+        public Maior()
+        {
+            this.n1 = 0;
+            this.n2 = 0;
+            this.resultado = "";
+
+        }
+
+        public void setn1(double n1)
+        {
+            this.n1 = n1;
+        }
+        public void setn2(double n2)
+        {
+            this.n2 = n2;
+        }
+        public double getn1()
+        {
+            return this.n1;
+        }
+        public double getn2()
+        {
+            return this.n2;
+        }
+
+        public string getresultado()
+        {
+            return this.resultado;
+        }
+
+        public void Comparar()
+        {
+            if (this.n1 > this.n2)
+            {
+                this.resultado = "O primeiro número é maior que o segundo";
+            }
+            else
+            {
+                if (this.n2 > this.n1)
+                {
+                    this.resultado = "O segundo número é maior queo primeiro";
+                }
+                else
+                {
+                    this.resultado = "Eles são iguais";
+                }
+
+            }
+        }
+    }
+}
+
---
+++ Maior/Maior/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maior
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maior num = new Maior();
+
+            num.setn1(msg("Digite o primeiro número: "));
+            num.setn2(msg("Digite o segundo número: "));
+
+            num.Comparar();
+            Console.WriteLine("{0}", num.getresultado());
+
+        }
+
+        static double msg(string msg)
+        { 
+            Console.Write(msg);
+            return double.Parse(Console.ReadLine());
+        }
+    }
+}
