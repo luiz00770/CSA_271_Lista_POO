f279348dd0749f0cf26284e59e0d0a5b223caf5f
luiz00770 <almeida.filipe@aluno.ifsp.edu.br>
2026-09-14 20:10:57 -0300

    Add files via upload

---
+++ Dólar_Real/Dólar_Real/Conversão.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dólar_Real
+{
+    internal class Conversão
+    {
+        private double cot;
+        private double quant;
+        private double resultado;
+        public Conversão()
+        {
+            this.cot = 0;
+            this.quant = 0;
+            this.resultado = 0;
+
+        }
+
+        public void setcot(double cot)
+        { 
+            this.cot = cot;
+        }
+        public void setquant(double quant)
+        { 
+            this.quant = quant;
+        }
+        public double getcot()
+        {
+            return this.cot;
+        }
+        public double getquant()
+        {
+            return this.quant;
+        }
+        public double getresultado()
+        {
+            return this.resultado;
+        }
+
+        public void Converter()
+        { 
+            this.resultado = this.cot*this.quant;
+        }
+    }
+}
---
+++ Dólar_Real/Dólar_Real/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dólar_Real
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conversão valor = new Conversão();
+
+            valor.setcot(Entrarint("Qual a cotação atual do dólar para o real? "));
+            valor.setquant(Entrarint("Quanto dólares você quer converter? "));
+
+            valor.Converter();
+            Console.WriteLine("Você possui {0} reais com essa quantidade de dólares", valor.getresultado());
+                
+
+        }
+        static double Entrarint(string mensagem)
+        {
+            Console.Write(mensagem);
+            return double.Parse(Console.ReadLine());
+        }
+    }
+}
