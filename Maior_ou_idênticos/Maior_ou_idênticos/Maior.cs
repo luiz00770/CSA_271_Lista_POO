07cd4de9966aacc45fbd72ee00ae3acbb6bb7896
luiz00770 <almeida.filipe@aluno.ifsp.edu.br>
2026-09-14 20:51:46 -0300

    Add files via upload

---
+++ Maior_ou_idênticos/Maior_ou_idênticos/Maior.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maior_ou_idênticos
+{
+    internal class Maior
+    {
+        private double n1;
+        private double n2;
+        private double resultado;
+
+
+
+        public Maior()
+        {
+            this.n1 = 0;
+            this.n2 = 0;
+            this.resultado = 0;
+
+        }
+
+        public void setn1(double n1)
+        {
+            this.n1 = n1;
+        }
+        public void setn2(double n2)
+        {
+            this.n2 = n2;
+        }
+        public double getn1()
+        {
+            return this.n1;
+        }
+        public double getn2()
+        {
+            return this.n2;
+        }
+
+        public double getresultado()
+        {
+            return this.resultado;
+        }
+
+        public void Comparar()
+        {
+            if (this.n1 > this.n2)
+            {
+                this.resultado = 1;
+            }
+            else
+            {
+                if (this.n2 > this.n1)
+                {
+                    this.resultado = 2;
+                }
+                else
+                {
+                    this.resultado = 3;
+                }
+
+            }
+        }
+
+    }
+}
---
+++ Maior_ou_idênticos/Maior_ou_idênticos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maior_ou_idênticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Maior n = new Maior();
+
+            n.setn1(msg("Digite o primeiro número: "));
+            n.setn2(msg("Digite o segundo número: "));
+
+            n.Comparar();
+            if (n.getresultado() == 1)
+            {
+                Console.WriteLine("O primeiro número é maior");
+            }
+            if (n.getresultado() == 2)
+            {
+                Console.WriteLine("O segundo número é maior");
+            }
+            if (n.getresultado() == 3)
+            {
+                Console.WriteLine("Os dois números são idênticos");
+            }
+
+
+
+        }
+        static double msg(string msg)
+        {
+            Console.Write(msg);
+            return double.Parse(Console.ReadLine());
+        }
+
+    }
+}
